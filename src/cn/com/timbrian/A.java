2c47873fbed34d0ea861e373a3bd12928e440782
TimBrian <1581149305@qq.com>
2019-03-22 12:47:29 +0800

    test

---
+++ src/cn/com/timbrian/A.java
@@ -0,0 +1,14 @@
+package cn.com.timbrian;
+
+public class A {
+	
+	static {
+		i = 1;
+	}
+	
+	static { 
+		int i = 3;
+	}
+	
+	static int i;
+}
---
+++ src/cn/com/timbrian/CharacterEncodingUtils.java
@@ -0,0 +1,16 @@
+package cn.com.timbrian;
+
+/**
+ * @author Tim Brian
+ */
+public class CharacterEncodingUtils {
+	
+	public static String iso8859_1ConvertUtf_8(String str) {
+		
+		return str;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
